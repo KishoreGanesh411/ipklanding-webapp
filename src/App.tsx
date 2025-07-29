9f75e5ce95df5cd0151c338bce94d1b5a76d3721
KishoreGanesh411 <kishoreganeshk14@gmail.com>
2025-07-29 17:10:36 +0530

    about commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-
-import "./index.css"
+import "./index.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import About from "./Components/About/About";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <About />
+      </div>
     </div>
   );
 };
